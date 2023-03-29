b7e322f2a9e310d6e102c4b7df1f3aacaaffe0b7
maimuna omar <maymunaabdi07@gmail.com>
2023-03-29 23:37:14 +0300

    completed assignments

---
+++ index.js
@@ -0,0 +1,22 @@
+function divide () {
+    return divide
+}
+
+
+function divide(){
+    let result =2000/100
+    return result
+}
+console.log(divide());
+
+
+let x = 2
+const square = (x) => {
+return x * x
+}
+
+let a =3
+let b =4
+const add = (a,b) => {
+    return a+b
+} 
